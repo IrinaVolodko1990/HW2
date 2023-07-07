384ca184ce683a60de68941a049bdd3b39aeb697
IrinaVolodko <8232734@gmail.com>
2023-07-07 17:15:40 +0300

    Day off task

--- Program.cs
+++ Program.cs
@@ -14,3 +14,25 @@ Console.WriteLine($"Second unit of this number is {dozens}");
 //int hundreds = number / 100; //вычисляем сотни
 //Console.Write (hundreds);
 
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.Write("Input number from 1 to 7: ");
+int day = Int32.Parse(Console.ReadLine());
+if (day >= 1 && day <= 5)
+{
+    Console.WriteLine("It's not a day off");
+}
+else if (day == 6 || day == 7)
+{ Console.WriteLine("It is a day off"); }
+else { Console.WriteLine("Wrong number"); }
